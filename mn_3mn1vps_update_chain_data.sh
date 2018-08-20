b57dc27e5427a3ee47062946967ac1d46e3f699f
Noom <asupawat@users.noreply.github.com>
2018-08-20 14:02:46 +0700

    Update mn_3mn1vps_update_chain_data.sh

--- mn_3mn1vps_update_chain_data.sh
+++ mn_3mn1vps_update_chain_data.sh
@@ -18,7 +18,7 @@ echo "**************************************************************************
 echo && echo
 
 cd ~
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/stamp.tar.gz
+sudo wget https://github.com/asupawat/stamp-test/releases/download/v1/stamp.tar.gz
 sudo tar -xzvf stamp.tar.gz
 sudo systemctl stop stampmn1
 sudo systemctl stop stampmn2
@@ -32,7 +32,7 @@ sudo rm ./stamp.tar.gz
 sudo rm -rf ./stamp
 
 
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/chain.tar.gz
+sudo wget https://github.com/asupawat/stamp-test/releases/download/v1/chain.tar.gz
 sudo tar -xzvf chain.tar.gz
 sudo rm ./chain.tar.gz
 
